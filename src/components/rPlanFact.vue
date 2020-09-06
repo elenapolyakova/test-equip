d7b719086c52fe71b94f0b62fe0ccafde3a168aa
elenapolyakova <65355090+elenapolyakova@users.noreply.github.com>
2020-09-06 15:54:35 +0300

    Update rPlanFact.vue

--- src/components/rPlanFact.vue
+++ src/components/rPlanFact.vue
@@ -16,7 +16,13 @@
       </div>
       <div class="report-content">
         <div class='title'>{{report_name}}</div>
-        <div>{{eqDataWork}}</div>
+         <div id ="chart" class="chart">
+          <div id="chartBar" class="chartBar">
+              <apexchart width="500" type="bar" :options="chartBarOptions" :series="seriesBar"></apexchart>
+              
+          </div>
+          <img id="imgBar" class="img-hidden"></img>
+      </div>
       </div>
   </div>
 </template>
@@ -31,6 +37,8 @@
    import ExcelJS from 'exceljs'; 
   import pdfMake from 'pdfmake/build/pdfmake'
   import pdfFonts from 'pdfmake/build/vfs_fonts'
+  import VueApexCharts from 'vue-apexcharts'
+   import {toCost} from '../utils/date'
 
    export default {
     name: "rPlanFact",
@@ -48,7 +56,37 @@
             isLoading: false,
          report_name: 'План и факт работы обуродования',
           fData: {},
-          eqDataWork: {}
+          eqDataWork: {},
+          hasLoadedImg: 0,
+          month: 8,
+          year: 2020,
+           labels: [],
+
+           chartBarOptions: {
+           chart: {
+             id: 'vuechart-example',
+             toolbar: {
+               tools:{
+                 download: false
+               }
+             }
+           },
+           xaxis: {
+             categories:  []
+           },
+            yaxis: {
+              title: {text: 'Время работы, ч' }
+           },
+            tooltip: {
+              enabled: false,
+              y: {
+                formatter: function (val) {
+                  return  val + ' ч.'
+                }
+              }
+           }
+         },
+         seriesBar: [{ name: '1', data: [] }],
        }
     },
     methods: {
@@ -76,8 +114,36 @@
               get('/rPlanFact/' + this.curEqId)
               .then(response => 
               {
-                this.eqDataWork = response.data;;
-                console.log(this.eqDataWork);
+                this.eqDataWork = response.data;
+
+                 this.hasLoadedImg = 0;
+                 
+                    var daysInMonth = new Date(this.year, this.month - 1, 1).daysInMonth();
+                let newData = Array(daysInMonth).fill(0);
+                this.labels = [];
+                for (var i = 1; i<=daysInMonth; i++ ){
+                    labels[i] = i.toString();
+                }
+            this.chartBarOptions = {...this.chartBarOptions, ...{
+                xaxis: {
+                    categories: this.labels
+                }
+            }}
+            
+          this.eqDataWork.forEach(item => {
+
+              newData[new Date(item.days).getDate()] = item.minute_count/60;
+           } ) 
+          this.seriesBar = [{
+              data: newData
+          }]
+        
+        
+           setTimeout(() => {
+               this.renderImage('chartBar', 'imgBar')
+              }
+              , 3000);
+
 
                 this.isLoading = false;
              })
@@ -89,16 +155,50 @@
                   
               })
 
-        }
+        },
+         renderImage: function(chartId, imgId){
+            var chart = document.getElementById(chartId);
+            var $chart = $(chart);
+            var img = document.getElementById(imgId);
+
+            var canvas = $($chart.find('.apexcharts-canvas')[0]);
+
+            $(img).width(canvas.width());
+
+             html2canvas(chart, {onclone: (document) =>{}} 
+                ).then((canvas) => {
+                  const dataURL = canvas.toDataURL('image/png');
+                 img.src = dataURL;
+               
+          });
 
     },
     created: function(){
        this.fData.eqIdQ = this.eqId;
+        window.html2canvas = html2canvas;
     }
 }
 </script>
 
 <style lang="scss" scoped>
+.chart{
+   display: flex;
+   flex-wrap: nowrap;
+  justify-content:center;
+}
+.chartBar{
+  margin-top: .5em;
+  width: 500px;
+}
+
+ img {
+    max-width: 100%;
+    height: auto;
+    width: auto\9; /* ie8 */
+ }
+ .img-hidden{
+   display: none;
+ }
 
 
 </style>
